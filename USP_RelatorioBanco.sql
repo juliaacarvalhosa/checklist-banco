996dd1f552af4fbc892ff48469ee2fadde91e0dd
Júlia Carvalhosa <20212105186@veigadealmeida.edu.br>
2024-06-05 14:51:29 -0300

    Update USP_RelatorioBanco.sql
    correção HTML

--- USP_RelatorioBanco.sql
+++ USP_RelatorioBanco.sql
@@ -17,19 +17,20 @@ DECLARE @Mensagem NVARCHAR(MAX);
 
 
 SET @Mensagem = '<html>
-					<head>
-						<style>
-							.online { color: green}
-						</style>
-					</head>
-						<body>
-							<h1>Verificação Banco de Dados:</h1>
-							<br></br>
-							<p>Todos os volumes de armazenamento estão operando com espaço adequado, todas as nossas bases de dados estão <span class="online">online</span> e não foi registrado erro em nenhum job.</p>
-							<br></br>
-							<img src="F:\CIS\Logotipos\TCE-RJ\STI_CIS.png" width="40%">
-						</body>
-				</html>'
+			<head>
+			<style>
+				.online { color: green}
+			</style>
+			</head>
+				<body>
+					<h1>Verificação Banco de Dados:</h1>
+					<br></br>
+					<p>Todos os volumes de armazenamento estão operando com espaço adequado, todas as nossas bases de dados estão <span class="online">online</span> e não foi registrado erro em nenhum job.</p>
+					<br></br>
+					<img src="F:\CIS\Logotipos\TCE-RJ\STI_CIS.png" width="40%">
+				</body>
+		</html>'
+	
     EXEC [dbo].[Alerta_Espaco_Disco]
         @email = '',
         @htmlOutput = @HtmlContent OUTPUT;
@@ -43,16 +44,19 @@ SET @Mensagem = '<html>
 
 
 IF @HtmlContent = '' AND @HtmlContent2 = '' AND @HtmlContent3 = ''
-	BEGIN
-		SET @FinalHtml = @Mensagem
+BEGIN
+	SET @FinalHtml = @Mensagem
 END
+	
 ELSE 
-	BEGIN
+BEGIN
 		SET @HtmlContent = CASE WHEN @HtmlContent = '' THEN 'Nenhum volume está com Epaço Livre abaixo do limite.' ELSE @HtmlContent END
 		SET @HtmlContent2 = CASE WHEN @HtmlContent2 = '' THEN 'Nenhuma base encontra-se <span style="color:red">OFFLINE</span>' ELSE @HtmlContent2 END
 		SET @HtmlContent3 = CASE WHEN @HtmlContent3 = '' THEN 'Nenhum Job com erro' ELSE @HtmlContent3 END
 
-		SET @FinalHtml ='<html>
+		SET @FinalHtml =
+	
+	'<html>
         <head>
             <style>
                 .container {
@@ -91,14 +95,14 @@ ELSE
         </body>
         </html>'
 
-	END
+END
 
-			EXEC msdb.dbo.sp_send_dbmail
-            @profile_name = '',
-            @recipients = '',
-            @subject = '',
-            @body = @FinalHtml,
-            @body_format = 'HTML'
+	EXEC msdb.dbo.sp_send_dbmail
+        @profile_name = '',
+        @recipients = '',
+        @subject = '',
+	@body = @FinalHtml,
+        @body_format = 'HTML'
 
 END
 GO
